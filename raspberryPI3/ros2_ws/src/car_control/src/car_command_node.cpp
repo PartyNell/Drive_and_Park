b328811d0ff380d741848aafd1733be263069db2
root <root@OrdinateurNell.>
2024-11-13 18:00:44 +0100

    complete a part of the car_command node information

--- raspberryPI3/ros2_ws/src/car_control/src/car_command_node.cpp
+++ raspberryPI3/ros2_ws/src/car_control/src/car_command_node.cpp
@@ -16,26 +16,53 @@ public:
     car_command()
     : Node("car_command_node")
     {
-        publisher_joystick_order_= this->create_publisher<interfaces::msg::JoystickOrder>("joystick_order", 10);
+        publisher_car_control= this->create_publisher<interfaces::msg::JoystickOrder>("car_command", 10);
+
+        subscription_joystick = this->create_subscription<interfaces::msg::JoystickOrder>("joystick_order", 10, std::bind(&car_command::carCommand, this, _1));
     }
 
 private:
-    void carCommandCallback()
+    void carCommand_JoystickOrder(const interfaces::msg::JoystickOrder & joystickOrder)
     {
-        auto message = std_msgs::msg::String();
-        message.data = "Hello, world! " + std::to_string(count_++);
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+        //if there is no obstacle detected then the message send is the one sending by the joystick
+        if(!obstacle_detection)
+        {
+            //Joystick order is sent
+            publisher_car_control->publish(joystickOrder);
+        } 
+        else
+        {
+            //The previous order from obstacle_detection is sent
+            publisher_car_control->publish(safety_order);
+        }
 
-        publisher_joystick_order_->publish(joystickOrderMsg); //Send order to the car_control_node
+
     }
 
-    rclcpp::Publisher<interfaces::msg::JoystickOrder>::SharedPtr publisher_joystick_order_;
+    void carCommand()
+    {
+        //set the detect obstacle variable depending on the data receved from the obstacle_detection
+    }
+
+    rclcpp::Publisher<interfaces::msg::JoystickOrder>::SharedPtr publisher_car_control;
+    bool obstacle_detection = false;
+    interfaces::msg::JoystickOrder safety_order = {
+        //start
+        //mode
+        //throttle
+        //steer
+        //reverse
+    }
+
+    //we should create 2 topics 1.(start and mode) and 2.(throttle, steer and reverse)
+    //1. Joystick send it message to car_control (we do not need to process those elements in car_command)
+    //2. Joystick send it message to car_command
 };
 
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<joystick_to_cmd>();
+    auto node = std::make_shared<car_command>();
 
     rclcpp::spin(node);
 
