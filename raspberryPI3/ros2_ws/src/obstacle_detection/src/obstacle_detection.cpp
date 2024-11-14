2f9beb728a562100b86dc7a7645529ffafc33275
Florian <floriancunnac@gmail.com>
2024-11-14 11:03:43 +0100

    add publisher to obstacle_detection.cpp v1.0

--- raspberryPI3/ros2_ws/src/obstacle_detection/src/obstacle_detection.cpp
+++ raspberryPI3/ros2_ws/src/obstacle_detection/src/obstacle_detection.cpp
@@ -8,6 +8,8 @@ public:
     {
         // Create a subscription to the "/us_data" topic
         subscription_ = this->create_subscription<interfaces::msg::Ultrasonic>("/us_data", 10, std::bind(&ObstacleDetection::topic_callback, this, std::placeholders::_1));
+        publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+        timer_ = this->create_wall_timer(500ms, std::bind(&ObstacleDetection::timer_callback, this));
     }
 
 private:
@@ -46,7 +48,20 @@ private:
         // Add further comparisons as needed
     }
 
+    void timer_callback()
+    {
+      auto message = std_msgs::msg::String();
+      message.data = "Hello, world! ";
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      publisher_->publish(message);
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
+
     rclcpp::Subscription<interfaces::msg::Ultrasonic>::SharedPtr subscription_;
+ 
 };
 
 int main(int argc, char *argv[])
