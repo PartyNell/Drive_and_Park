2d0a4a6bcdb2809d975f757986d4f25841f2c657
najines <inem.09@orange.fr>
2024-12-13 11:36:13 +0100

    Code tested : the car stops after x meters

--- raspberryPI3/ros2_ws/src/drive_autonomously/src/autonomous_driving.cpp
+++ raspberryPI3/ros2_ws/src/drive_autonomously/src/autonomous_driving.cpp
@@ -10,7 +10,7 @@
 
 #define PULSE_FOR_A_REVOLUTION 36
 #define WHEEL_DIAMETER 20.0
-#define MAX_DISTANCE 1.0
+#define MAX_DISTANCE 100 //in centimeters
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -24,9 +24,9 @@ class AutonomousDriving : public rclcpp::Node
       publisher_car_order_ = this->create_publisher<interfaces::msg::JoystickOrder>("autonomous_car_order", 10);
       publisher_init_state_ = this->create_publisher<std_msgs::msg::Bool>("start_init", 10);
 
-      subscriber_autonomous_mode_ = this->create_subscription<interfaces::msg::JoystickOrder>("joystick_order", 10, std::bind(&AutonomousDriving::init_autonomous_mode, this, _1));
-      subscriber_init_ok_ = this->create_subscription<std_msgs::msg::Bool>("init_finished", 10, std::bind(&AutonomousDriving::start_straight, this, _1));
-	  subscription_motors_feedback_ = this->create_subscription<interfaces::msg::MotorsFeedback>("motors_feedback", 10, std::bind(&AutonomousDriving::computeDistance, this, _1));
+      subscriber_autonomous_mode_ = this->create_subscription<interfaces::msg::JoystickOrder>("joystick_order", 10, std::bind(&AutonomousDriving::test_distance, this, _1));
+      //subscriber_init_ok_ = this->create_subscription<std_msgs::msg::Bool>("init_finished", 10, std::bind(&AutonomousDriving::start_straight, this, _1));
+	    subscription_motors_feedback_ = this->create_subscription<interfaces::msg::MotorsFeedback>("motors_feedback", 10, std::bind(&AutonomousDriving::computeDistance, this, _1));
 
       timer_ = this->create_wall_timer(50ms, std::bind(&AutonomousDriving::timer_callback, this));
 
@@ -34,58 +34,80 @@ class AutonomousDriving : public rclcpp::Node
     }
 
   private:
-    void init_autonomous_mode(const interfaces::msg::JoystickOrder & joystick){
+//     void init_autonomous_mode(const interfaces::msg::JoystickOrder & joystick){
+//       if(mode != joystick.mode){
+//         mode = joystick.mode;
+
+//         if(mode == 1){
+//             //std_msgs::msg::Bool init_autonomous;
+//             //init_autonomous.data = true;
+//             //publisher_init_state_->publish(init_autonomous);
+
+//             init_in_progress = true;
+//             search_in_progress = false;
+//         } else if (mode != 1 && init_in_progress){
+//             std_msgs::msg::Bool init_autonomous;
+//             init_autonomous.data = false; 
+//             publisher_init_state_->publish(init_autonomous);
+
+//             init_in_progress = false;
+//             search_in_progress = false;
+//         }
+//       }
+//     }
+
+//     void start_straight(const std_msgs::msg::Bool & init){
+//         //if(init.data){
+//         //  RCLCPP_INFO(this->get_logger(), "Initialisation DONE");
+
+//           car_order.start = true;
+//           car_order.mode = 1;
+//           car_order.throttle = 0.7;
+//           car_order.steer = 0.0;
+//           car_order.reverse = false;
+
+//           search_in_progress = true;
+
+//         //} else {
+//         //  RCLCPP_INFO(this->get_logger(), "Initialisation FAILED");
+// //
+//         //  //STOP the car
+//         //  car_order.start = true;
+//         //  car_order.mode = 1;
+//         //  car_order.throttle = 0.0;
+//         //  car_order.steer = 0.0;
+//         //  car_order.reverse = false;
+// //
+//         //  search_in_progress = false;
+//         //}
+//     }
+
+    void test_distance(const interfaces::msg::JoystickOrder & joystick){
       if(mode != joystick.mode){
         mode = joystick.mode;
 
         if(mode == 1){
-            std_msgs::msg::Bool init_autonomous;
-            init_autonomous.data = true;
-            publisher_init_state_->publish(init_autonomous);
+            //std_msgs::msg::Bool init_autonomous;
+            //init_autonomous.data = true;
+            //publisher_init_state_->publish(init_autonomous);
+            distance_travelled = 0.0;
 
-            init_in_progress = true;
-            search_in_progress = false;
-        } else if (mode != 1 && init_in_progress){
-            std_msgs::msg::Bool init_autonomous;
-            init_autonomous.data = false; 
-            publisher_init_state_->publish(init_autonomous);
+            car_order.start = true;
+            car_order.mode = 1;
+            car_order.throttle = 0.7;
+            car_order.steer = 0.0;
+            car_order.reverse = false;
 
             init_in_progress = false;
-            search_in_progress = false;
+            search_in_progress = true;
         }
       }
-    }
-
-    void start_straight(const std_msgs::msg::Bool & init){
-        if(init.data){
-          RCLCPP_INFO(this->get_logger(), "Initialisation DONE");
-
-          car_order.start = true;
-          car_order.mode = 1;
-          car_order.throttle = 0.7;
-          car_order.steer = 0.0;
-          car_order.reverse = false;
-
-          search_in_progress = true;
-
-        } else {
-          RCLCPP_INFO(this->get_logger(), "Initialisation FAILED");
-
-          //STOP the car
-          car_order.start = true;
-          car_order.mode = 1;
-          car_order.throttle = 0.0;
-          car_order.steer = 0.0;
-          car_order.reverse = false;
-
-          search_in_progress = false;
-        }
     }
 
 
 	void computeDistance(const interfaces::msg::MotorsFeedback & motorsFeedback)
 	{
-		if (search_in_progress)
+		if (search_in_progress && mode == 1)
 		{
 			
       distance_to_add = motorsFeedback.left_rear_odometry*WHEEL_DIAMETER*M_PI/PULSE_FOR_A_REVOLUTION;
@@ -97,7 +119,14 @@ class AutonomousDriving : public rclcpp::Node
 			{
 				// The speed sent to the control of the car is 0
 				car_order.throttle = 0.0;
-				RCLCPP_INFO(this->get_logger(), "The car has driven %.2f meters.", distance_travelled);
+				RCLCPP_INFO(this->get_logger(), "The car has driven %.2f centimeters.", distance_travelled);
+
+        if (init_in_progress){
+            std_msgs::msg::Bool init_autonomous;
+            init_autonomous.data = false; 
+            publisher_init_state_->publish(init_autonomous);
+            init_in_progress = false; 
+        }
 			}
 		}
 	}
@@ -116,7 +145,6 @@ class AutonomousDriving : public rclcpp::Node
     rclcpp::Publisher<interfaces::msg::JoystickOrder>::SharedPtr publisher_car_order_;
     rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr publisher_init_state_;
 
-    //Subscribers
     rclcpp::Subscription<interfaces::msg::JoystickOrder>::SharedPtr subscriber_autonomous_mode_;
     rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr subscriber_init_ok_;
 	  rclcpp::Subscription<interfaces::msg::MotorsFeedback>::SharedPtr subscription_motors_feedback_;
